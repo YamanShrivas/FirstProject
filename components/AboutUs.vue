71ca4dc9f6e4e5aab23af24673ec238a16c376e6
YamanShrivas <yaman98416@gmail.com>
2024-07-31 00:36:23 +0530

    Edited the component header

--- components/AboutUs.vue
+++ components/AboutUs.vue
@@ -7,7 +7,7 @@
           <h1
             class="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4"
           >
-      Our Story
+       Story
           </h1>
           <div class="flex  justify-center">
             <div class="w-16 h-1 rounded-full bg-indigo-500 inline-flex"></div>
